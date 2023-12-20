cb77de3faf3f75fdf7ebde5a8ccb7a46be356a50
Fabian Habermann <mailfuerfabian@web.de>
2023-12-20 21:06:08 +0100

    big update, probably first realy usable Qt-Ui version

--- catvibes.py
+++ catvibes.py
@@ -1,17 +1,19 @@
-import os
-from pathlib import Path
 import curses
-import shutil
+import sys
+import os
 
 import catvibes_lib as lib
 
 lib.init()
 
+for arg in sys.argv[1:]:
+    if arg == "--gui":
+        os.system("python qt_gui.py")
+        sys.exit(0)
+
 config = lib.config
 playlists = lib.playlists
 song_data = lib.song_data
-
-
 
 
 def ui(screen):
@@ -24,57 +26,56 @@ def ui(screen):
     curses.use_default_colors()
 
     # first value is space from top, second from bottom
-    y_restrictions = (2,1)
+    y_restrictions = (2, 1)
     playlist_screen = screen.derwin(maxy - sum(y_restrictions), maxx, y_restrictions[0], 0)
-    music_player_screen = screen.derwin(maxy,0)
+    music_player_screen = screen.derwin(maxy, 0)
 
-    tabs = [lib.SongsTab(playlist_screen)]
+    tabs: list[lib.DisplayTab] = [lib.SongsTab(playlist_screen)]
     tabs.extend(
-        [lib.PlaylistTab(playlist_screen,name,playlist) for name,playlist in playlists.val.items()]
-        )
+        [lib.PlaylistTab(playlist_screen, name, playlist) for name, playlist in playlists.val.items()]
+    )
     tab = 0
 
-    lib.music_player = lib.music_player_with_screen(music_player_screen)
-
+    lib.music_player = lib.MusicPlayerWithScreen(music_player_screen)
 
     def tabbar():
         """draws the tabbar and seperator lines"""
         cursor = 0
         for t in tabs:
             if tabs[tab] == t:
-                screen.addstr(0,cursor,t.title,curses.A_REVERSE)
+                screen.addstr(0, cursor, t.title, curses.A_REVERSE)
             else:
-                screen.addstr(0,cursor,t.title)
-            cursor += len(t.title)+4
+                screen.addstr(0, cursor, t.title)
+            cursor += len(t.title) + 4
             if t != tabs[-1]:
-                screen.addstr(0,cursor - 2 , "│")
+                screen.addstr(0, cursor - 2, "│")
 
-        screen.hline(1,0, curses.ACS_HLINE, maxx)
-        screen.hline(maxy - 1,0,curses.ACS_HLINE, maxx)
+        screen.hline(1, 0, curses.ACS_HLINE, maxx)
+        screen.hline(maxy - 1, 0, curses.ACS_HLINE, maxx)
 
     def resize():
         nonlocal maxx, maxy
         maxy, maxx = screen.getmaxyx()
         maxy, maxx = maxy - 1, maxx - 1
         playlist_screen.resize(maxy - sum(y_restrictions), maxx)
-        music_player_screen.resize(1,maxx)
-        music_player_screen.mvwin(maxy,0)
-
+        music_player_screen.resize(1, maxx)
+        music_player_screen.mvwin(maxy, 0)
 
     tabbar()
     tabs[tab].disp()
     key = " "
-    while key not in ("q", "\x1b"):   # UI mainloop
+    while key not in ("q", "\x1b"):  # UI mainloop
         if key == "KEY_RIGHT":
-            tab = (tab +1) % len(tabs)
+            tab = (tab + 1) % len(tabs)
         elif key == "KEY_LEFT":
-            tab = (tab -1) % len(tabs)
+            tab = (tab - 1) % len(tabs)
         elif key == "l":
             name = lib.inputstr(playlist_screen, "Name of the playlist: ")
-            temp = lib.Pointer([])
-            lib.data.load(lib.playlist_dir.joinpath(name),temp, default=[])
-            playlists.val[name] = temp
-            tabs.append(lib.PlaylistTab(playlist_screen, name, temp))
+            if name is not None:
+                temp = lib.Pointer([])
+                lib.data.load(lib.playlist_dir.joinpath(name), temp, default=[])
+                playlists.val[name] = temp
+                tabs.append(lib.PlaylistTab(playlist_screen, name, temp))
         else:
             tabs[tab].handle_key(key)
         tabs[tab].disp()
@@ -94,8 +95,7 @@ def ui(screen):
         screen.timeout(-1)
 
 
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     try:
         curses.wrapper(ui)
     finally:
